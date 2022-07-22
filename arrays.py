72578c438188097fe75f603dced9e79bb1c2fdd6
IsuruLakshan170CSFT <Isuru.K@CreativeSoftware.com>
2022-07-23 01:42:53 +0530

    array py

---
+++ arrays.py
@@ -0,0 +1,53 @@
+# # one dimensional example
+# from numpy import array
+# # list of data
+# data = [11, 22, 33, 44, 55]
+# # array of data
+# data = array(data)
+# print(data)
+# print(type(data))
+
+# # two dimensional example
+# from numpy import array
+# # list of data
+# data = [[11, 22],
+# 		[33, 44],
+# 		[55, 66]]
+# # array of data
+# data = array(data)
+# print(data)
+# print(type(data))
+
+# # simple indexing
+# from numpy import array
+# # define array
+# data = array([11, 22, 33, 44, 55])
+# # index data
+# print(data[-1])
+# print(data[-5])
+
+# # 2d indexing
+# from numpy import array
+# # define array
+# data = array([[11, 22], [33, 44], [55, 66]])
+# # index data
+# print(data[0,0])
+
+# # simple slicing
+# from numpy import array
+# # define array
+# data = array([11, 22, 33, 44, 55])
+# print(data[2:])
+
+# split input and output
+from numpy import array
+# define array
+data = array([[11, 22, 33],
+		[44, 55, 66],
+		[77, 88, 99]])
+# separate data
+X, y = data[:, :-1], data[:, -1]
+print(X)
+print(y)
+
+
