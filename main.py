a5f72ba72d4637aaaa1a8be39aa7462ae5b83ef1
IsuruLakshan170CSFT <Isuru.K@CreativeSoftware.com>
2022-07-23 00:49:31 +0530

    add plots

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
 
-def print_hi(name):
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
 
-    print(f'Hi, {name}')
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+print(dataset)
+# print(dataset.shape)
+# print(dataset.head(20))
+#
+print(dataset.describe())
+#
+# print(dataset.groupby('class').size())
 
+# box and whisker plots
+# dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+# pyplot.show()
 
+# histograms
+# dataset.hist()
+# pyplot.show()
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-
+# scatter plot matrix
+scatter_matrix(dataset)
+pyplot.show()
