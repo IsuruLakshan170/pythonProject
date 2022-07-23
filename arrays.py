4ed54c63cf1ac835dfc4271729c1afb3ed27aee5
IsuruLakshan170CSFT <Isuru.K@CreativeSoftware.com>
2022-07-23 11:54:43 +0530

    array py reshape

--- arrays.py
+++ arrays.py
@@ -38,16 +38,36 @@
 # # define array
 # data = array([11, 22, 33, 44, 55])
 # print(data[2:])
+#
+# # split input and output
+# from numpy import array
+# # define array
+# data = array([[11, 22, 33],
+# 		[44, 55, 66],
+# 		[77, 88, 99]])
+# # separate data
+# X, y = data[:, :-1], data[:, -1]
+# print(X)
+# print(y)
 
-# split input and output
+# # split train and test
+# from numpy import array
+# # define array
+# data = array([[11, 22, 33],
+# 		[44, 55, 66],
+# 		[77, 88, 99]])
+# # separate data
+# split = 2
+# train,test = data[:split,:],data[split:,:]
+# print(train)
+# print(test)
+
+# array shape
 from numpy import array
-# define array
-data = array([[11, 22, 33],
-		[44, 55, 66],
-		[77, 88, 99]])
-# separate data
-X, y = data[:, :-1], data[:, -1]
-print(X)
-print(y)
-
-
+# list of data
+data = [[11, 22],
+		[33, 44],
+		[55, 66]]
+# array of data
+data = array(data)
+print(data.shape)
